9a37923c20dce1b21ff56fab51cf7b3cbe62ca54
thomas <toto@tot.fr>
2021-06-01 14:29:34 +0200

    init

---
+++ testSpringBootFin/src/main/java/org/m2i/TestSpringBootFinApplication.java
@@ -0,0 +1,13 @@
+package org.m2i;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootFinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBootFinApplication.class, args);
+	}
+
+}
---
+++ testSpringBootFin/src/test/java/org/m2i/TestSpringBootFinApplicationTests.java
@@ -0,0 +1,13 @@
+package org.m2i;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBootFinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
